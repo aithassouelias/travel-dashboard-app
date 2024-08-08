7cc3c903d699ac141894f753ffc319b796b601a8
Elias AIT HASSOU <116345081+aithassouelias@users.noreply.github.com>
2024-08-08 16:14:56 +0200

    Border map added

--- map.py
+++ map.py
@@ -1,13 +1,23 @@
 import folium
 
 # Créer une carte centrée sur une latitude et une longitude spécifiques
-m = folium.Map(location=[45.5236, -122.6750], zoom_start=13, tiles='cartodbdark_matter')
+m = folium.Map(zoom_start=13, tiles='cartodbdark_matter')
 
-# Ajouter un marqueur
-folium.Marker(
-    location=[45.5236, -122.6750],
-    icon=folium.Icon(icon="glyphicon glyphicon-heart", color="black", icon_color="#FFD700"),
-).add_to(m)
+locations = [
+    ("Tour Eiffel, Paris", [48.8584, 2.2945]),
+    ("Londres", [51.5074, -0.1278]),
+    ("Marrakech", [31.6295, -7.9811]),
+    ("Stade Vélodrome, Marseille", [43.2699, 5.3958]),
+    ("Bordeaux", [44.8378, -0.5792])
+]
+
+for location in locations: 
+    # Ajouter un marqueur
+    folium.Marker(
+        location=location[1],
+        popup=location[0],
+        icon=folium.Icon(icon="glyphicon glyphicon-heart", color="black", icon_color="#FFD700"),
+    ).add_to(m)
 
 # Enregistrer la carte en tant que fichier HTML
 m.save('map.html')
